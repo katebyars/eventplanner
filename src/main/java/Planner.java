9db46eb824f5abff30c6506b65696e2da9b77fc2
Kate Byars <katebyars5@gmail.com>
2017-08-04 09:16:30 -0700

    add method to calculate total party price.

--- src/main/java/Planner.java
+++ src/main/java/Planner.java
@@ -8,12 +8,12 @@ public class Planner {
 
     private String nameofParty;
     private int numberOfGuests;
-    private ArrayList<String> foodItems;
-    private ArrayList<String> beverages;
     private String bandName;
 
     public Planner(String nameofParty, int numberOfGuests, String bandName){
         this.nameofParty = nameofParty;
+        this.numberOfGuests = numberOfGuests;
+        this.bandName = bandName;
     }
 
     public String getBandName() {
@@ -25,11 +25,43 @@ public class Planner {
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
-    public ArrayList<String> getFoodItems() {
-        return foodItems;
-    }
-    public ArrayList<String> getBeverages() {
-        return beverages;
+
+    public int calculatePartyPrice(int numberOfGuests, String foodMenu, String bandName, String beverageMenu){
+        int totalPrice = 0;
+        int foodPrice = 0;
+        int bandPrice = 0;
+        int beveragePrice = 0;
+
+        if (foodMenu.equals("1")){
+            foodPrice = numberOfGuests*20;
+        }
+        else if (foodMenu.equals("2")){
+            foodPrice = numberOfGuests*30;
+        }
+        else if (foodMenu.equals("3")){
+            foodPrice = numberOfGuests*40;
+        }
+        if (bandName.equals("1")){
+            bandPrice = 2000;
+        }
+        else if (bandName.equals("2")){
+            bandPrice = 3000;
+        }
+        else if (bandName.equals("3")){
+            bandPrice = 4000;
+        }
+        if (beverageMenu.equals("1")) {
+            beveragePrice = numberOfGuests*5;
+        }
+        else if (beverageMenu.equals("2")){
+            beveragePrice = numberOfGuests*12;
+        }
+        else if (beverageMenu.equals("3")){
+            beveragePrice = numberOfGuests*35;
+        }
+
+        totalPrice = ((numberOfGuests*10) + foodPrice + bandPrice + beveragePrice);
+        return totalPrice;
     }
 
 }
