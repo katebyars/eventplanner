97114ea5e26a30ae2b433b3b6a351c9e85320f9e
Kate Byars <katebyars5@gmail.com>
2017-08-04 09:54:28 -0700

    correct errors in UI for buffered reader and inputStreamReader.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,14 +9,16 @@ import models.Planner;
  */
 public class App {
     public static void main(String[] args) {
-        Planner planner = new Planner();
-        BufferedReader bufferedReader = new BufferedReader(InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Hello and welcome to the party planner! Let's go over a few questions so that we can work up a custom quote for you.");
+        try {
+
         System.out.println("For our records, what is the name of your party?");
         String nameofParty = bufferedReader.readLine();
 
         System.out.println("How many guests will you have?");
-        int numberOfGuests = bufferedReader.readLine();
+        int numberOfGuests = Integer.parseInt(bufferedReader.readLine());
 
         System.out.println("We have several menus to choose from. Please type in the number of the menu you would like: 1. Deluxe International 2. American Fusion Buffet 3. Backyard Barbeque");
         String foodMenu = bufferedReader.readLine();
@@ -27,6 +29,10 @@ public class App {
         System.out.println("Would you like beverages at your party? We offer many choices. Please type the number of your choice: 1.Soft Drinks 2.Beer and Wine 3.Full Well Bar, Unlimited");
         String beverageMenu = bufferedReader.readLine();
 
+
+            Planner planner = new Planner(nameofParty, numberOfGuests, bandName, foodMenu, beverageMenu);
+
+        }
         catch(IOException e)
         {
             e.printStackTrace();
